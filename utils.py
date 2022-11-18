3d64075db9b4733e83f2cdfb4d0a660fda804497
Jacob <jw.braidley@gmail.com>
2022-11-18 12:49:41 +0000

    completed maxvalue() plus new method for exceptions

--- utils.py
+++ utils.py
@@ -10,10 +10,10 @@ def sumvalues(values):
         values -- the list/array of values"""
     sum = 0
     for item in values:
-        if type(item) != int:
-            raise Exception("All values in sequence must be numerical. Wrong data type given.")
-        else:
+        if type(item) == int or type(item) == float or type(item) == complex: #TODO: better syntax than this
             sum += item
+        else:
+            raise Exception("All values in sequence must be numerical. Wrong data type given.")
     return sum
 
 
@@ -21,8 +21,20 @@ def maxvalue(values):
     """Receives a list/array, returning the index of the maximum value in that sequence.
     
     Keyword arguments:
-        values -- the list/array of values"""    
-    ## Your code goes here
+        values -- the list/array of values"""
+    maxIndex = 0
+    max = 0
+    i = 0
+    while i < len(values):
+        #TODO: raise exception for nonnumerical
+        if type(values[i]) == int or type(values[i]) == float or type(values[i]) == complex: #TODO: better syntax than this
+            if values[i] > max:
+                maxIndex = i
+                max = values[i]
+            i += 1
+        else:
+            raise Exception("All values in sequence must be numerical. Wrong data type given.")
+    return maxIndex
 
 
 def minvalue(values):
