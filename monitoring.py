cd4b70bab0f3b64ec41d8c074625ababe6bf9f95
Jacob <jw.braidley@gmail.com>
2022-12-15 01:16:14 +0000

    Update monitoring.py

--- monitoring.py
+++ monitoring.py
@@ -10,8 +10,8 @@
 
 def get_live_data_from_api(site_code='MY1',species_code='NO',start_date=None,end_date=None):
     """
-    Return data from the LondonAir API using its AirQuality API. 
-    
+    Return data from the LondonAir API using its AirQuality API.
+
     *** This function is provided as an example of how to retrieve data from the API. ***
     It requires the `requests` library which needs to be installed. 
     In order to use this function you first have to install the `requests` library.
@@ -21,10 +21,10 @@ def get_live_data_from_api(site_code='MY1',species_code='NO',start_date=None,end
     import datetime
     start_date = datetime.date.today() if start_date is None else start_date
     end_date = start_date + datetime.timedelta(days=1) if end_date is None else end_date
-    
-    
+
+
     endpoint = "https://api.erg.ic.ac.uk/AirQuality/Data/SiteSpecies/SiteCode={site_code}/SpeciesCode={species_code}/StartDate={start_date}/EndDate={end_date}/Json"
-   
+
     url = endpoint.format(
         site_code = site_code,
         species_code = species_code,
